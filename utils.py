742ddb49d64dd667dd01d51ccc399e4e249f21ad
Alexandre-Luft-Duarte <alexandreduarte43234@gmail.com>
2025-04-02 10:47:08 -0300

    mudanças

--- utils.py
+++ utils.py
@@ -1,7 +1,5 @@
 import csv
 import os
-import time
-import pyautogui
 
 def ler_codigos_csv(caminho_csv):
     """
@@ -12,24 +10,8 @@ def ler_codigos_csv(caminho_csv):
     codigos = []
     if not os.path.exists(caminho_csv):
         raise FileNotFoundError(f"Arquivo não encontrado: {caminho_csv}")
-    with open(caminho_csv, "r") as arquivo:
+    with open(caminho_csv, "r", encoding="utf-8") as arquivo:
         reader = csv.DictReader(arquivo, delimiter=';')
         for row in reader:
             codigos.append(row['imovel_prefeitura'])
     return codigos
-
-def localizar_imagem(imagem, tentativas=5, intervalo=1, confidence=0.8):
-    """
-    Tenta localizar uma imagem na tela várias vezes, aguardando um intervalo entre as tentativas.
-    :param imagem: Caminho da imagem para localizar.
-    :param tentativas: Número máximo de tentativas.
-    :param intervalo: Tempo de espera entre as tentativas (em segundos).
-    :param confidence: Nível de confiança para a localização.
-    :return: Posição central da imagem se encontrada, ou None.
-    """
-    for _ in range(tentativas):
-        posicao = pyautogui.locateCenterOnScreen(imagem, confidence=confidence)
-        if posicao:
-            return posicao
-        time.sleep(intervalo)
-    return None
